b3f13db9eca3d4d27b8e9b628627f7d682b76b5b
unknown <samantha.c.egge@gmail.com>
2019-07-11 14:50:46 -0700

    Added additional styling. Properly imported AntD css to scss file.

--- src/components/PhotoDisplay/PhotoDisplay.js
+++ src/components/PhotoDisplay/PhotoDisplay.js
@@ -43,14 +43,20 @@ const PhotoDisplay = () => {
               size={'large'} />
               {/* replace h2 with {photo.title} */}
             <h2>SUN SPOT</h2>
-            {/* replace src with {photo.url} */}
-            <img src='https://timedotcom.files.wordpress.com/2014/10/1966875_733873019993257_6346729834986608387_n.jpg' alt="nasa pic" />
 
-            <Card style={{ width: 900, marginTop: 16 }} loading={loading}>
+            <Card style={{ width: 900, marginTop: 16 }} loading={loading}
+              cover={             
+                // replace src with {photo.url}
+                <img
+                  alt="Nasa Photo of The Day"
+                  src="https://timedotcom.files.wordpress.com/2014/10/1966875_733873019993257_6346729834986608387_n.jpg"
+                />
+              }>
+
               <Meta
                 title="Description"
-                description='REPLACE THIS WITH {photo.explanation} But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'
               />
+              <p>'REPLACE THIS WITH {photo.explanation} But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'</p>
             </Card>
         </div>
     )
