f028af9169ce9bcf1ced6ccb90ad31c0e97bc028
unknown <samantha.c.egge@gmail.com>
2019-07-11 15:21:23 -0700

    Snapshot testing working.

--- src/components/PhotoDisplay/PhotoDisplay.js
+++ src/components/PhotoDisplay/PhotoDisplay.js
@@ -8,9 +8,9 @@ import './PhotoDisplay.scss'
 
 
 const PhotoDisplay = () => {
-    const [photo, setPhoto] = useState([]);
+    const [photo, setPhoto] = useState(null);
     const [loading, setLoading] = useState(false)
-    const { MonthPicker, RangePicker } = DatePicker;
+    const [date, setDate] = useState('2012/03/14')
 
     const dateFormat = 'YYYY/MM/DD';
     const { Meta } = Card;
@@ -30,14 +30,23 @@ const PhotoDisplay = () => {
     //   fetchPhoto();
     // }, []);
 
-    const onChange = checked => {
-      setLoading(true);
-    };
+    function onChange(date, dateString) {
+      console.log(date, dateString);
+      setDate(dateString)
+    }
+
+    // if (!photo) {
+    //   setLoading(true)
+    // }
+    // else {
+    //   setLoading(false)
+    // }
 
     return (
         <div className="photo-display">
             <h2>Pick a Date</h2>
             <DatePicker 
+              onChange={onChange}
               defaultValue={moment('2012/03/14', dateFormat)} 
               format={dateFormat}
               size={'large'} />
@@ -56,7 +65,8 @@ const PhotoDisplay = () => {
               <Meta
                 title="Description"
               />
-              <p>'REPLACE THIS WITH {photo.explanation} But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'</p>
+              {/* {photo.explanation} */}
+              <p>'REPLACE THIS WITH  But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'</p>
             </Card>
         </div>
     )
